22fa98c620994ef11f9ad78b6e986a1f8f96b306
Shreyasj1508 <119724567+Shreyasj1508@users.noreply.github.com>
2024-05-30 13:37:16 +0000

    Maths in JS

---
+++ 01_basics/06_Maths.js
@@ -0,0 +1,29 @@
+// console.log(Math);
+// console.log(Math.abs(-45));
+// //
+// console.log(Math.round(23.4));
+// console.log(Math.round(23.49));
+// console.log(Math.round(23.5));
+// console.log(Math.round(23.500));
+// console.log(Math.round(23.6));
+// //
+// console.log(Math.floor(23.49));
+// console.log(Math.ceil(23.49));
+// //
+// console.log(Math.min(1 ,2,3,4,4,5,6));
+// console.log(Math.max(1 ,2,3,4,4,5,6));
+// //
+// console.log(Math.pow(2,4));
+// console.log(Math.sqrt(121));
+//
+//
+console.log(Math.random()); // 0 - 1
+console.log(Math.random()*10 + 1 );  // min value will be 1
+console.log(Math.floor(Math.random()*10) + 1);  //
+//
+//
+//
+const min = 10
+const max = 20
+console.log(Math.random() * (max - min + 1)) 
+console.log(Math.floor(Math.random() * (max - min + 1))  + min);
