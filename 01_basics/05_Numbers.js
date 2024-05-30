5f1f23e88d6bacda83bab13392e8622225a0ccce
Shreyasj1508 <119724567+Shreyasj1508@users.noreply.github.com>
2024-05-30 13:36:16 +0000

    Number in JS

---
+++ 01_basics/05_Numbers.js
@@ -0,0 +1,30 @@
+const score = 500
+console.log(score);
+//
+//
+//
+ // includes some properties
+// const balance = new Number(100)
+// console.log(balance);
+//
+//
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(5));
+//
+//
+const number2 = 698.444555
+console.log(number2.toPrecision(2));
+console.log(number2.toPrecision(3));
+console.log(number2.toPrecision(4));
+console.log(number2.toPrecision(5));
+//
+//
+// 
+const hundreds = 10000000000
+// US standards
+console.log(hundreds.toLocaleString());
+// Indian Standards
+console.log(hundreds.toLocaleString('en-IN'));
+//
+//
+//
