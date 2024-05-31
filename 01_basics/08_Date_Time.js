a1128a41c086de8f89295630cad1479b5d061a76
Shreyas Jaiswal <119724567+Shreyasj1508@users.noreply.github.com>
2024-05-31 12:22:51 +0000

    Date and Time in JS

---
+++ 01_basics/08_Date_Time.js
@@ -0,0 +1,41 @@
+// let dateNow = new Date()
+// //console.log(dateNow.toString());
+// console.log(dateNow.toDateString());
+// console.log(dateNow.toLocaleDateString());
+// console.log(typeof dateNow); // Object
+//
+///
+//
+let myCreatedDate1 = new Date(2024 , 4 , 31)
+let myCreatedDate2 = new Date(2024, 1, 26 , 4,5)
+let myCreatedDate3 = new Date("2024-01-25")
+let myCreatedDate4 = new Date("03-05-2024")
+// console.log(myCreatedDate1.toLocaleDateString());
+//  console.log(myCreatedDate2.toLocaleString());
+console.log(myCreatedDate3.toLocaleString());
+
+// console.log(myCreatedDate1.toDateString());
+// console.log(myCreatedDate2.toDateString());
+// console.log(myCreatedDate3.toDateString());
+//
+//
+//
+// let myCreatedDate5 = new Date("03-05-2024")
+// let myTimeStamp = Date.now()
+// console.log(myTimeStamp);
+// console.log(myCreatedDate5.getTime());
+//
+//
+//
+// let myCreatedDate6 = new Date("03-05-2024")
+// let myTimeStamp2 = Date.now()
+// console.log(Math.floor(Date.now()/1000));
+//
+//
+
+let myCreatedDate7 = new Date("12-18-2024")
+let myTimeStamp3 = Date.now()
+console.log(myCreatedDate7.getMonth());
+console.log(myCreatedDate7.getDay());
+
+
