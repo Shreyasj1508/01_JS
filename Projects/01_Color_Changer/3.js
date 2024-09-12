88cf04474107bdeba95fd20686b084c9ad3effc4
Shreyas Jaiswal <119724567+Shreyasj1508@users.noreply.github.com>
2024-09-12 15:33:40 +0000

    project 1 updated

--- Projects/01_Color_Changer/3.js
+++ Projects/01_Color_Changer/3.js
@@ -1,38 +1,32 @@
 const buttons = document.querySelectorAll('.button');
 const body = document.querySelector('body');
+
 buttons.forEach(function (button) {
-  console.log(button);
   button.addEventListener('click', function (e) {
-    console.log(e);
-    console.log(e.target);
-    if (e.target.id === 'grey') {
-      body.style.backgroundColor = e.target.id;
+   
+
+    if(e.target.id === 'red'){
+      body.style.backgroundColor = 'red'
+      // if original color can be directly written
     }
-    if (e.target.id === 'white') {
-      body.style.backgroundColor = e.target.id;
+    if(e.target.id === 'orange'){
+      body.style.backgroundColor = e.target.id
+      // if original color can be directly written as this as well
     }
-    if (e.target.id === 'blue') {
-      body.style.backgroundColor = e.target.id;
+    if(e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id
     }
-    if (e.target.id === 'yellow') {
-      body.style.backgroundColor = e.target.id;
-    }
-    if (e.target.id === 'red') {
-      body.style.backgroundColor = e.target.id;
+    if(e.target.id === 'green'){
+    //  body.style.backgroundColor = e.target.id
+     body.style.backgroundColor = 'rgb(39, 255, 1)';
     }
     if (e.target.id === 'pink') {
-      body.style.backgroundColor = e.target.id;
-    }
-
-    if (e.target.id === 'orange') {
-      body.style.backgroundColor = e.target.id;
-    }
-
-    if (e.target.id === 'green') {
-      body.style.backgroundColor = e.target.id;
+      body.style.backgroundColor = 'rgb(236, 106, 128)'
     }
     if (e.target.id === 'brown') {
-      body.style.backgroundColor = e.target.id;
+      body.style.backgroundColor = 'rgb(124, 28, 28)';
     }
+
+
   });
 });
